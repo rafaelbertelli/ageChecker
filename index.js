3b3f251a38ce7bae44486c52e94cf7ef448a61cc
Rafael Borges <rafaelbertelli@gmail.com>
2019-04-27 22:41:39 -0300

    Trying out some middleware

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const app = express()
-app.use(express.urlencoded({ extended: false })) // allow to receive data from a form
-app.set('view engine', 'njk')
 
 nunjucks.configure('views', {
   autoescape: true,
@@ -11,8 +9,43 @@ nunjucks.configure('views', {
   watch: true
 })
 
+app.use(express.urlencoded({ extended: false })) // allow to receive data from a form
+app.set('view engine', 'njk')
+
+const checkAgeQueryParam = (req, res, next) => {
+  const { age } = req.query
+
+  if (!age) {
+    return res.redirect('/')
+  }
+
+  return next()
+}
+
 app.get('/', (req, res) => {
-  return res.send('Hello, Rafinha!!!')
+  return res.render('form')
+})
+
+app.get('/major', checkAgeQueryParam, (req, res) => {
+  const { age } = req.query
+
+  return res.render('major', { age })
+})
+
+app.get('/minor', checkAgeQueryParam, (req, res) => {
+  const { age } = req.query
+
+  return res.render('minor', { age })
+})
+
+app.post('/check', (req, res) => {
+  const { age } = req.body
+
+  if (age >= 18) {
+    return res.redirect(`/major?age=${age}`)
+  }
+
+  return res.redirect(`/minor?age=${age}`)
 })
 
 app.listen(3000)
